bf86e300e07618767d1059472bb6c27eb7232061
Pim Snel <post@pimsnel.com>
2023-03-18 11:36:53 +0100

    It works\!

--- src/teledream.cr
+++ src/teledream.cr
@@ -29,17 +29,48 @@ module Teledream
       end
     end
 
-    private def get_ai_resp(message : String | Nil) : String
-      if message.nil?
-        return "no valid prompt"
+#    private def get_dir(message: String) : String
+#
+#    end
+
+    private def get_ai_resp(message) : String
+      msg = message.text
+      if msg.nil?
+        return "No valid prompt"
+
       else
-        command = "/home/pim/.conda/envs/ldm/bin/python /home/pim/cSD/stable-diffusion/optimizedSD/optimized_txt2img.py --turbo --H 512 --W 768 --n_iter 1 --n_samples 4 --ddim_steps 50 --prompt \""+message+"\""
-        #command = "/home/pim/.conda/envs/ldm/bin/python --version"
+        command = "/home/pim/.conda/envs/ldm/bin/python /home/pim/cSD/stable-diffusion/optimizedSD/optimized_txt2img.py --turbo --H 512 --W 768 --n_iter 1 --n_samples 4 --ddim_steps 50 --prompt \""+msg+"\""
+#        File.open("/tmp/teledream.lock", "w") do |file|
+#            file.print message
+#        end
+
         Process.run("sh", {"-c", command}) do |proc|
-#        Process.run(command) do |proc|
             puts proc.output.gets
+#            File.delete("/tmp/teledream.lock")
         end
-        return command
+
+        lastdirpart = msg.gsub('|','_').gsub(' ','_').gsub(':','_')[0..150]
+        sample_path = File.join(["/home/pim/cSD/stable-diffusion/outputs/txt2img-samples", lastdirpart  ])
+        p sample_path
+
+        #sample_path = os.path.join(outpath, "_".join(re.split(":| ", prompts[0])))[:150]
+
+        media_group = [] of InputMediaPhoto
+
+        d = Dir.new(sample_path)
+        d.each_child { |filename|
+          p filename
+          fullpath = File.expand_path(filename, sample_path)
+          newfile = InputMediaPhoto.new(
+            media: fullpath,
+            caption: "Local"
+          )
+          media_group << newfile
+        }
+
+        message.reply_with_media_group(media_group)
+
+        return "Done dreaming..."
       end
     end
 
@@ -51,6 +82,13 @@ module Teledream
     @[On(:message)]
     def on_message(update)
       return unless message = update.message
+
+#      if File.exists?("/tmp/teledream.lock")
+#        content = File.open("/tmp/teledream.lock") do |file|
+#          file.gets_to_end
+#        end
+#        return "Busy with: " + content
+#      end
 
       puts "#{message.message_id}: Message recieved: #{message.text}"
 
@@ -66,7 +104,7 @@ module Teledream
         return
       end
 
-      ai_response = get_ai_resp(message.text)
+      ai_response = get_ai_resp(message)
       puts "#{message.message_id}: Message reply: #{ai_response}"
       message.reply(ai_response, parse_mode: ParseMode::HTML)
     end
